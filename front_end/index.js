c74ae1c020a4b4f4d510a11b21e9565e48517a5b
vienpham2019 <vienpham2019@gmail.com>
2020-03-24 17:14:49 -0500

    update alien

--- front_end/index.js
+++ front_end/index.js
@@ -9,6 +9,9 @@ const GAME_HEIGHT = window.innerHeight
 const PLAYER_MAX_SPEED = 15
 const lazerSpeed = 6
 
+const AMOUNT_ALIEN = 5
+const SPACE_BETWEEN = parseInt(GAME_WIDTH / AMOUNT_ALIEN) / 2
+
 let player_width
 let lazerCooldown = lazerSpeed
 
@@ -18,7 +21,8 @@ const GAME_STATE = {
     mousePress: false,
     playerX: 0,
     playerY: 0,
-    lazers: []
+    lazers: [],
+    enemies: [],
 }
 
 function setPosition(element,x,y) {
@@ -26,20 +30,34 @@ function setPosition(element,x,y) {
     element.style.top = `${y}px`
 }
 
-function createPlayer(container) {
-
+function createPlayer() {
     let player = document.createElement("img")
     player.src = "images/pngguru.com.png"
     player.id = "space_ship"
     container.append(player)
     player_width = player.width
     GAME_STATE.playerX = GAME_WIDTH / 2 - (player_width / 2)
-    GAME_STATE.playerY = GAME_HEIGHT - 110
+    GAME_STATE.playerY = GAME_HEIGHT - player_width
     setPosition(player,GAME_WIDTH.playerX,GAME_STATE.playerY)
+}
+
+function createAlien(i) {
+    let alien = document.createElement("img")
+    alien.src = "images/alien.png"
+    alien.className = "alien_ship"
+    let x = SPACE_BETWEEN * i 
+    let y = 0
+    setPosition(alien,x,y)
+    container.append(alien)
+    GAME_STATE.enemies.push({alien ,x ,y })
 }
 
 function init() {
     createPlayer(container)
+
+    for(let i = 1; i <= AMOUNT_ALIEN ; i ++ ){
+        createAlien(i)
+    }
 }
 
 function updatePlayer() {
@@ -54,7 +72,7 @@ function updatePlayer() {
 
 }
 
-function createLazer(container, x , y) {
+function createLazer(x , y) {
     x = x + (player_width / 2)
     let lazer = document.createElement("img")
     lazer.src = "images/laserBlue16.png"
@@ -69,16 +87,38 @@ function updateLazers() {
     for(let i = 0; i < lazers.length; i++){
         let lazer = lazers[i]
         lazer.y -= 20
-        lazer.y > 0 ? setPosition(lazer.lazer,lazer.x,lazer.y) : lazer.lazer.remove()
+        if(lazer.y < 100){
+            checkPosition(lazer)
+        }
+        if(lazer.y > 0){
+            setPosition(lazer.lazer,lazer.x,lazer.y)
+        }else{
+            lazer.lazer.remove()
+            lazers.splice(lazers.indexOf(lazer),1)
+        }
+    }
+}
+
+function checkPosition(element) { 
+    let enemies = GAME_STATE.enemies
+    for (let i = 0; i < enemies.length; i++) {
+        let alien = GAME_STATE.enemies[i]
+        let x1 = alien.x 
+        let x2 = alien.x + 100 
+        if(x1 < element.x && x2 > element.x){
+            alien.alien.remove()
+            enemies.splice(enemies.indexOf(alien),1)
+            element.lazer.remove()
+            GAME_STATE.lazers.splice(GAME_STATE.lazers.indexOf(element),1)
+        }
     }
 }
 
 function update() {
-    const container = document.querySelector(".game")
     updatePlayer()
     updateLazers()
     if(GAME_STATE.mousePress && lazerCooldown <= 0){
-        createLazer(container,GAME_STATE.playerX,GAME_STATE.playerY)
+        createLazer(GAME_STATE.playerX,GAME_STATE.playerY)
         lazerCooldown = lazerSpeed
     }
 
